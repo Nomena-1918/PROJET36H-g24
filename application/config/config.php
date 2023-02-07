78949cc4f7ed3fe3364cdb8fc448c973e64ac431
Nomena-1918 <119030942+Nomena-1918@users.noreply.github.com>
2023-02-07 17:10:41 +0300

    Pour online

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/PROJET36H-g24-TEST/';
-//$config['base_url'] = 'http://groupe24.alwaysdata.net/PROJET36H-g24/';
+//$config['base_url'] = 'http://localhost/PROJET36H-g24-TEST/';
+$config['base_url'] = 'http://groupe24.alwaysdata.net/PROJET36H-g24/';
 
 /*
 |--------------------------------------------------------------------------
