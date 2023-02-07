cdf0a63ca8fa5d98db0a6013541e894dc4380d81
Nomena-1918 <119030942+Nomena-1918@users.noreply.github.com>
2023-02-07 16:23:49 +0300

    Debut header

--- application/views/header.php
+++ application/views/header.php
@@ -5,6 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="<?php echo site_url('assets/bootstrap/css/bootstrap.min.css'); ?>">
+    <!--
+        <script src="<?php echo site_url('assets/bootstrap/js/bootstrap.min.js'); ?>"></script>
+    -->
     <title>LogAdmin</title>
 </head>
 <body>
