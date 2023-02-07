4d150d5deeff394a458116a3829779188ee9fa7c
Renato2508 <119660851+Renato2508@users.noreply.github.com>
2023-02-07 16:44:26 +0300

    ajout de footer et config

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,3 +1,11 @@
 <?php
     defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
+    <footer class="bg-light py-3">
+        <div class="container">
+            <p class="text-center">Renato ETU 1830    ---   Nomena ETU 1918 ­</p>
+            <p class="text-center">­Copyright &copy; 2023 Mon site web</p>
+        </div>
+    </footer>
+</body>
+</html>
